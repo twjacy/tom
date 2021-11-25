4fd7338a824235c081fa65672ff7d6537a060f1e
twjacy <84982878+twjacy@users.noreply.github.com>
2021-11-25 20:28:13 +0800

    backup

---
+++ BOXJS/boxjs.js
@@ -0,0 +1,191 @@
+{
+    "id": "xl2101200.app.sub",
+        "name": "tom 脚本订阅",
+            "author": "@tom",
+                "icon": "https://gitee.com/tom210120/tom/raw/master/Tom/tom.png",
+                    "repo": "https://github.com/xl2101200/-",
+                        "apps": [
+                            {
+                                "id": "sjb",
+                                "name": "睡觉宝",
+                                "keys": ["sjbheader", "sjbheader2", "sjbheader3", "sjbheader4", "sjbheader5", "sjbheader6", "sjbheader7", "sjbheader8", "sjbheader9", "sjbheader10", "sjbheader11", "sjbheader12", "sjbheader13", "sjbheader14", "sjbheader15", "sjbheader16", "sjbheader17", "sjbheader18", "sjbheader19", "sjbheader20"],
+                                "author": "@tom",
+                                "settings": [{
+                                    "id": "sjbSuffix",
+                                    "name": "当前账号",
+                                    "val": "1",
+                                    "type": "number",
+                                    "desc": "当前抓取ck记录的账号序号，如：1、2、3、4"
+                                }, {
+                                    "id": "sjbCount",
+                                    "name": "账号个数",
+                                    "val": "1",
+                                    "type": "number",
+                                    "desc": "指定任务最多跑几个账号，根据抓取的账号数据个数来设值"
+                                }, {
+                                    "id": "sjbXH",
+                                    "name": "循环获取ck",
+                                    "val": "0",
+                                    "type": "number",
+                                    "desc": "0关闭，1打开，默认关闭"
+                                }, {
+                                    "id": "sjbTXTX",
+                                    "name": "txtx",
+                                    "val": "0",
+                                    "type": "number",
+                                    "desc": "0关闭，1打开，默认关闭"
+                                }, {
+                                    "id": "sjbSC",
+                                    "name": "sc",
+                                    "val": "0",
+                                    "type": "number",
+                                    "desc": "0关闭，1打开，默认关闭"
+                                }, {
+                                    "id": "sjbnotifyttt",
+                                    "name": "推送控制",
+                                    "val": "1",
+                                    "type": "number",
+                                    "desc": "0关闭，1推送,默认12点以及23点推送"
+                                }, {
+                                    "id": "sjbnotifyInterval",
+                                    "name": "通知控制",
+                                    "val": "2",
+                                    "type": "number",
+                                    "desc": "0关闭，1为 所有通知，2为 12，23 点通知，3为 6，12，18，23 点通知"
+                                }, {
+                                    "id": "sjbMinutes",
+                                    "name": "推送-通知 分钟控制",
+                                    "val": "10",
+                                    "type": "number",
+                                    "desc": "推送以及通知控制在什么分钟段，可设置0-59,默认0到10"
+                                }],
+                                "repo": "https://gitee.com/tom210120/tom/raw/master/SJB/sjb.js",
+                                "icons": ["https://gitee.com/tom210120/tom/raw/master/Tom/Tom.jpg", "https://gitee.com/tom210120/tom/raw/master/Tom/Tom.jpg"],
+                                "script": "https://gitee.com/tom210120/tom/raw/master/SJB/sjb.js",
+                                "icon": "https://gitee.com/tom210120/tom/raw/master/Tom/Tom.jpg"
+                            },
+                            {
+                                "id": "qkhq",
+                                "name": "趣看行情",
+                                "keys": ["qkhqheader", "qkhqheader2", "qkhqheader3", "qkhqheader4", "qkhqheader5", "qkhqheader6", "qkhqheader7", "qkhqheader8", "qkhqheader9", "qkhqheader10", "qkhqheader11", "qkhqheader12", "qkhqheader13", "qkhqheader14", "qkhqheader15", "qkhqheader16", "qkhqheader17", "qkhqheader18", "qkhqheader19", "qkhqheader20"],
+                                "author": "@tom",
+                                "settings": [{
+                                    "id": "qkhqSuffix",
+                                    "name": "当前账号",
+                                    "val": "1",
+                                    "type": "number",
+                                    "desc": "当前抓取ck记录的账号序号，如：1、2、3、4"
+                                }, {
+                                    "id": "qkhqCount",
+                                    "name": "账号个数",
+                                    "val": "1",
+                                    "type": "number",
+                                    "desc": "指定任务最多跑几个账号，根据抓取的账号数据个数来设值"
+                                }, {
+                                    "id": "qkhqXH",
+                                    "name": "循环获取ck",
+                                    "val": "0",
+                                    "type": "number",
+                                    "desc": "0关闭，1打开，默认关闭"
+                                }, {
+                                    "id": "qkhqTXTX",
+                                    "name": "txtx",
+                                    "val": "0",
+                                    "type": "number",
+                                    "desc": "0关闭，1打开，默认关闭"
+                                }, {
+                                    "id": "qkhqSC",
+                                    "name": "sc",
+                                    "val": "0",
+                                    "type": "number",
+                                    "desc": "0关闭，1打开，默认关闭"
+                                }, {
+                                    "id": "qkhqnotifyttt",
+                                    "name": "推送控制",
+                                    "val": "1",
+                                    "type": "number",
+                                    "desc": "0关闭，1推送,默认12点以及23点推送"
+                                }, {
+                                    "id": "qkhqnotifyInterval",
+                                    "name": "通知控制",
+                                    "val": "2",
+                                    "type": "number",
+                                    "desc": "0关闭，1为 所有通知，2为 12，23 点通知，3为 6，12，18，23 点通知"
+                                }, {
+                                    "id": "qkhqMinutes",
+                                    "name": "推送-通知 分钟控制",
+                                    "val": "10",
+                                    "type": "number",
+                                    "desc": "推送以及通知控制在什么分钟段，可设置0-59,默认0到10"
+                                }],
+                                "repo": "https://gitee.com/tom210120/tom/raw/master/QKHQ/qkhq.js",
+                                "icons": ["https://gitee.com/tom210120/tom/raw/master/Tom/Tom.jpg", "https://gitee.com/tom210120/tom/raw/master/Tom/Tom.jpg"],
+                                "script": "https://gitee.com/tom210120/tom/raw/master/QKHQ/qkhq.js",
+                                "icon": "https://gitee.com/tom210120/tom/raw/master/Tom/Tom.jpg"
+                            },
+                            {
+                                "id": "jscfz",
+                                "name": "嘉实宠粉站",
+                                "keys": ["jscfzheader", "jscfzheader2", "jscfzheader3", "jscfzheader4", "jscfzheader5", "jscfzheader6", "jscfzheader7", "jscfzheader8", "jscfzheader9", "jscfzheader10", "jscfzheader11", "jscfzheader12", "jscfzheader13", "jscfzheader14", "jscfzheader15", "jscfzheader16", "jscfzheader17", "jscfzheader18", "jscfzheader19", "jscfzheader20"],
+                                "author": "@tom",
+                                "settings": [{
+                                    "id": "jscfzSuffix",
+                                    "name": "当前账号",
+                                    "val": "1",
+                                    "type": "number",
+                                    "desc": "当前抓取ck记录的账号序号，如：1、2、3、4"
+                                }, {
+                                    "id": "jscfzCount",
+                                    "name": "账号个数",
+                                    "val": "1",
+                                    "type": "number",
+                                    "desc": "指定任务最多跑几个账号，根据抓取的账号数据个数来设值"
+                                }, {
+                                    "id": "jscfzXH",
+                                    "name": "循环获取ck",
+                                    "val": "0",
+                                    "type": "number",
+                                    "desc": "0关闭，1打开，默认关闭"
+                                }, {
+                                    "id": "jscfzTXTX",
+                                    "name": "txtx",
+                                    "val": "0",
+                                    "type": "number",
+                                    "desc": "0关闭，1打开，默认关闭"
+                                }, {
+                                    "id": "jscfzSC",
+                                    "name": "sc",
+                                    "val": "0",
+                                    "type": "number",
+                                    "desc": "0关闭，1打开，默认关闭"
+                                }, {
+                                    "id": "jscfznotifyttt",
+                                    "name": "推送控制",
+                                    "val": "1",
+                                    "type": "number",
+                                    "desc": "0关闭，1推送,默认12点以及23点推送"
+                                }, {
+                                    "id": "jscfznotifyInterval",
+                                    "name": "通知控制",
+                                    "val": "2",
+                                    "type": "number",
+                                    "desc": "0关闭，1为 所有通知，2为 12，23 点通知，3为 6，12，18，23 点通知"
+                                }, {
+                                    "id": "jscfzMinutes",
+                                    "name": "推送-通知 分钟控制",
+                                    "val": "10",
+                                    "type": "number",
+                                    "desc": "推送以及通知控制在什么分钟段，可设置0-59,默认0到10"
+                                }],
+                                "repo": "https://gitee.com/tom210120/tom/raw/master/JSCFZ/jscfz.js",
+                                "icons": ["https://gitee.com/tom210120/tom/raw/master/Tom/Tom.jpg", "https://gitee.com/tom210120/tom/raw/master/Tom/Tom.jpg"],
+                                "script": "https://gitee.com/tom210120/tom/raw/master/JSCFZ/jscfz.js"
+                            }
+
+
+
+
+
+
+                        ]
+}
